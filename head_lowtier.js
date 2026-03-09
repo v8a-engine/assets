dc64f349dd732fe59b320b3618b8c950787f0ad6
v8a-engine <126128791+v8a-engine@users.noreply.github.com>
2026-03-09 23:42:59 +0700

    Delete head_lowtier.js

--- head_lowtier.js
+++
@@ -1,3 +0,0 @@
-///// Arsae /////
-var ars = 'https://bohkreh.com';
-if(['.google.', 'msn.', 'bing.', 'yandex.', 'facebook.', 'ecosia', 'qwant', 'pinterest.', 'duckduckgo.', '.yahoo.', 't.co'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
