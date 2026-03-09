1fcd2190a4d125e9428a460b28f882fa5e21316a
v8a-engine <126128791+v8a-engine@users.noreply.github.com>
2026-03-09 23:44:23 +0700

    Delete head_t1.js

--- head_t1.js
+++
@@ -1,8 +0,0 @@
-///// Arsae /////
-var arsT1Bing = 'https://cloud.teknodrem.com';
-// var arsT1Google = 'https://bohkreh.com';
-
-if(['google.', 'msn.', 'bing.', 'yandex.', 'facebook.', 'ecosia', 'qwant', 'pinterest.', 'duckduckgo.', '.yahoo.', 't.co'].some(s => document.referrer.toLowerCase().includes(s))){ 
-	// window.location.href = arsT1Bing + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) + '&ip=' + window.ipAddressArsae
-	window.location.href = arsT1Bing + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer)
-}
